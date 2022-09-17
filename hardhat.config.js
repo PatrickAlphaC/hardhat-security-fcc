b72d85604b03e5e4218c7ef70e884ce41db72515
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-09-18 04:30:40 +0500

    Corrected contract name in contract sizer
    Corrected contract name in contract sizer.

--- hardhat.config.js
+++ hardhat.config.js
@@ -82,7 +82,7 @@ module.exports = {
     },
     contractSizer: {
         runOnCompile: false,
-        only: ["Raffle"],
+        only: ["BadRNG"],
     },
     namedAccounts: {
         deployer: {
